ac61e3608c9699c4f73d0a0afb7ba423ff29b4d8
Fluff <dune@fluffigt.net>
2013-09-27 21:41:30 +0200

    Added mockup of json object

---
+++ web/mockup/js/stageobject.js
@@ -0,0 +1,34 @@
+[
+    {
+	"id": 12345,
+	"title": "Fluffs fantastiska konstverk",
+	"artist": "Fluff",
+	"year": "2013",
+	"material": "Pure Fluff",
+	"place": "Copenhagen",
+	"geodata": {
+	    "lat": "12.57431",
+	    "lng": "55.67443"
+	},
+	"photolink": "http://www.fluff.se/prof.jpg",
+	"photolic": "CC-BY-SA",
+	"text": "Detta är ett helt makalöst konstverk",
+	"textlic": "CC-BY-SA"
+    },
+    {
+	"id": 12346,
+	"title": "Andrés fantastiska konstverk",
+	"artist": "André",
+	"year": "2013",
+	"material": "Pure Andre",
+	"place": "Copenhagen",
+	"geodata": {
+	    "lat": "12.57451",
+	    "lng": "55.67493"
+	},
+	"photolink": "http://www.fluff.se/prof.jpg",
+	"photolic": "CC-BY-SA",
+	"text": "Detta är ett helt makalöst konstverk",
+	"textlic": "CC-BY-SA"
+    }
+]
