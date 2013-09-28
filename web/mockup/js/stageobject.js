0861cb3ce34c3b2034a80a9ab8d9e658b7c5e431
lokal-profil <lokal.profil@gmail.com>
2013-09-28 11:59:48 +0200

    New header stageobject

--- web/mockup/js/stageobject.js
+++ web/mockup/js/stageobject.js
@@ -1,60 +1,77 @@
 [
-    {
-	"id": 12345,
-	"title": "Fluffs fantastiska konstverk",
-	"artist": "Fluff",
-	"year": "2013",
-	"material": "Pure Fluff",
-	"place": "Copenhagen",
-	"geodata": {
-	    "lat": "55.67443",
-	    "lon": "12.77451"
-	},
-        "media": {
-            "mediatype" : "image",
-            "thumb": "http://www.fluff.se/prof.jpg",
-            "medialink" : "http://www.fluff.se/prof2.jpg",
- 	    "medialic": "CC-BY-SA",
-            "byline" : "By: Fluff / CC-BY-SA"
-        },
-        "text": {
-	    "fulltext": "Detta är ett helt makalöst konstverk",
-	    "textlic": "CC-BY-SA",
-	    "byline": "By: Fluff / CC-BY-SA"
-        },
-        "meta": {
-            "module": "stage",
-            "datalic": "PD",
-            "byline": "PD by xyz"
-	}
+    {"header":{
+        "st":"artist",
+        "q":"Carl",
+        "m":[
+            "stage",
+            "odok"
+        ],
+        "hits": 2,
+        "info":"No objects found for odok.",
+        "warning":"More than 500 hits",
+        "error":Null,
     },
-    {
-	"id": 12346,
-	"title": "Andrés fantastiska konstverk",
-	"artist": "André",
-	"year": "2013",
-	"material": "Pure Andre",
-	"place": "Copenhagen",
-	"geodata": {
-	    "lat": "55.67493",
-	    "lon": "12.57451"
-	},
-        "media": {
-            "mediatype" : "image",
-            "thumb": "http://www.fluff.se/prof.jpg",
-            "medialink" : "http://www.fluff.se/prof2.jpg",
-            "medialic": "CC-BY-SA",
-            "byline" : "By: Fluff / CC-BY-SA"
-        },
-        "text": {
-            "fulltext": "Detta är ett helt makalöst konstverk",
-            "textlic": "CC-BY-SA",
-            "byline" : "By: Fluff / CC-BY-SA"
-        },
-        "meta": {
-            "module": "stage",
-            "datalic": "PD",
-	    "byline": "PD by xyz"
-	}
+    {"body":
+        [
+            {
+                "id": 12345,
+                "title": "Fluffs fantastiska konstverk",
+                "artist": "Fluff",
+                "year": "2013",
+                "material": "Pure Fluff",
+                "place": "Copenhagen",
+                "geodata": {
+                    "lat": "55.67443",
+                    "lon": "12.77451"
+                },
+                "media": {
+                    "mediatype" : "image",
+                    "thumb": "http://www.fluff.se/prof.jpg",
+                    "medialink" : "http://www.fluff.se/prof2.jpg",
+                "medialic": "CC-BY-SA",
+                    "byline" : "By: Fluff / CC-BY-SA"
+                },
+                "text": {
+                "fulltext": "Detta är ett helt makalöst konstverk",
+                "textlic": "CC-BY-SA",
+                "byline": "By: Fluff / CC-BY-SA"
+                },
+                "meta": {
+                    "module": "stage",
+                    "datalic": "PD",
+                    "byline": "PD by xyz"
+                }
+            },
+            {
+                "id": 12346,
+                "title": "Andrés fantastiska konstverk",
+                "artist": "André",
+                "year": "2013",
+                "material": "Pure Andre",
+                "place": "Copenhagen",
+                "geodata": {
+                    "lat": "55.67493",
+                    "lon": "12.57451"
+                },
+                "media": {
+                    "mediatype" : "image",
+                    "thumb": "http://www.fluff.se/prof.jpg",
+                    "medialink" : "http://www.fluff.se/prof2.jpg",
+                    "medialic": "CC-BY-SA",
+                    "byline" : "By: Fluff / CC-BY-SA"
+                },
+                "text": {
+                    "fulltext": "Detta är ett helt makalöst konstverk",
+                    "textlic": "CC-BY-SA",
+                    "byline" : "By: Fluff / CC-BY-SA"
+                },
+                "meta": {
+                    "module": "stage",
+                    "datalic": "PD",
+                "byline": "PD by xyz"
+                }
+            }
+        ]
     }
 ]
+    
