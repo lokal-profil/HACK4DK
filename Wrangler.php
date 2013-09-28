e3047080b32b45ed79768f543838b775ac93df92
lokal-profil <lokal.profil@gmail.com>
2013-09-28 11:01:29 +0200

    Initial commit

---
+++ Wrangler.php
@@ -0,0 +1,84 @@
+<?php
+include('Modules.php');
+class wrangler {
+    /**
+     * Api which passes on the js queries to various modules and returns their responses
+     * Needs to:
+     * read parameters from js query
+     * perform http post requests
+     * get basic info from modules (names, licenses, supported types etc.
+     * return json comprising of all search results
+     */
+    public static $availableModules = array( //the only place where you need to add new modules
+        'odok',
+        'voreskunst',
+    );
+    public static $thumb_width=100; //thumb_width
+    
+    // a list of the available modules including a longer linked name
+    public function get_availableModules(){
+        $available = array();
+        foreach (self::$availableModules as $moduleName){
+            $a = array(
+                'short_name'=>$moduleName,
+                'long_name'=>'<a href="' . $moduleName::$info_link .'">'. $moduleName::$long_name .'</a>',
+            );
+            array_push($available, $a);
+        }
+        self::respond($available);
+    }
+    
+    //Returns an array of modules supporting a certain type
+    public function supports_type($type){
+        $supported = array();
+        foreach (self::$availableModules as $moduleName){
+            if (in_array($type,$moduleName::$supported_types)){
+                array_push($supported, $moduleName);
+            }
+        }
+        self::respond($supported);
+    }
+    
+    //given type, value and modules to be included this collects all responses
+    public function make_queries($type, $value, $includedModules){
+        $includedModules = is_null($includedModules) ? self::$availableModules : $includedModules;
+        $results = array();
+        foreach (self::$availableModules as $moduleName){
+            if (in_array($moduleName, $includedModules)){
+                $mod = new $moduleName(self::$thumb_width);
+                $queryUrl = $mod->make_query($type, $value);
+                //TMP
+                echo $moduleName . ' : ' .$queryUrl . '<br>';
+                //END TMP
+                $response = self::make_httpRequest($queryUrl);
+                //tmp
+                echo 'hello from post httpRequest:' . substr($response, 0, 100) . '...<br>';
+                //end tmp
+                $problems = $mod->process_response($response);
+                if (!$problems){
+                    array_push($results, $mod->items);
+                } else {
+                    echo $problems;
+                }
+            }
+        }
+        self::respond($results);
+    }
+    
+    //turns reply into json for the js
+    private function respond($payload){
+        $json = json_encode($payload);
+        echo $json;
+    }
+    
+    //quick httpRequest - extend
+    private function make_httpRequest($queryUrl){
+        if (($file = file_get_contents($queryUrl)) === FALSE) {
+            echo("error");
+            $file=NULL;
+        }
+        return $file;
+    }
+
+}
+?>
---
+++ listener.php
@@ -0,0 +1,23 @@
+<?php
+include('Wrangler.php');
+/**
+ * Listener. If action=query
+ *  make_queries($type, $value, $includedModules)
+ */
+if(isset($_GET['action'])){
+    switch ($_GET['action']) {
+        case 'query' : 
+            wrangler::make_queries($_GET['st'],$_GET['q'],$_GET['m']);
+            break;
+        case 'allModules' :
+            wrangler::get_availableModules();
+            break;
+        case 'hasType' :
+            wrangler::supports_type($_GET['st']);
+            break;
+        default:
+            echo 'invalid action';
+            break;
+    }
+}
+?>
